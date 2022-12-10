fb45de6dc44dda6711d1d29316df0b0601b2c888
Bipasha2026 <119729925+Bipasha2026@users.noreply.github.com>
2022-12-10 15:59:44 +0530

    #include<stdio.h> int main() {     int n,i;     int esum=0,osum=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {             esum=esum+a[i];         }         else         {             osum=osum+a[i];         }     }     {         if(esum>osum)         {             printf("%d",esum-osum);
            }
            else
            {
               printf("%d",osum-esum);
            }
        }
    }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    int esum=0,osum=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            esum=esum+a[i];
+        }
+        else
+        {
+            osum=osum+a[i];
+        }
+    }
+    {
+        if(esum>osum)
+        {
+            printf("%d",esum-osum);
+            
+        }
+        else
+        {
+           printf("%d",osum-esum);
+        }
+    }
+}
