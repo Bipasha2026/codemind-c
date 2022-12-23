8f1f3bcc559f130cf9530580ae726c3e8f002cac
Bipasha2026 <119729925+Bipasha2026@users.noreply.github.com>
2022-12-23 23:01:47 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     while(a!=0 && b!=0)     {         if(a>b)         {             a=a%b;
            }
            else
            {
                b=b%a;
                
            }
        }
        if(a==0)
        printf("%d",b);
        else
        printf("%d",a);
    }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    while(a!=0 && b!=0)
+    {
+        if(a>b)
+        {
+            a=a%b;
+    
+        }
+        else
+        {
+            b=b%a;
+            
+        }
+    }
+    if(a==0)
+    printf("%d",b);
+    else
+    printf("%d",a);
+}
